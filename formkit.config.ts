cff948d4bcb53e256e5631d725c84c760963ef36
Julia Zuleger <julia.zuleger@init.de>
2024-06-18 15:25:11 +0200

    add form inputs

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,11 +1,15 @@
 import { genesisIcons } from "@formkit/icons"
-import { rootClasses } from "./formkit.theme"
+import { de, en } from '@formkit/i18n'
 import { createProPlugin, inputs } from '@formkit/pro'
+
+import { rootClasses } from "./formkit.theme"
 
 const pro = createProPlugin('fk-a3b7684ab0', inputs)
 
 const config = {
   plugins: [pro],
+  locales: { de, en },
+  locale: 'de',
   icons: { ...genesisIcons },
   config: { rootClasses }
 }
--- src/App.vue
+++ src/App.vue
@@ -7,59 +7,107 @@ async function submit() {
 
 <template>
   <div class="bg-white rounded-xl shadow-xl p-8 mx-auto my-16 max-w-[450px]">
-    <img
-      src="https://pro.formkit.com/logo.svg"
-      alt="FormKit Logo"
-      width="244"
-      height="50"
-      class="mx-auto mb-8 w-48"
-    >
+   
+       <!-- <button class="border-2 px-2 py-1 mb-4" @click="changeLocale('en')">
+      Show in Englisch
+    </button> -->
+
+    <h1 class="text-2xl mb-4">Sommerfest Skills</h1>
+
     <FormKit
       type="form"
+      :config="{ validationVisibility: 'live' }"
       #default="{ value }"
       @submit="submit"
     >
+      <!-- <FormKit type="text" label="name" /> -->
       <FormKit
         type="text"
         name="name"
-        label="Name"
-        help="What do people call you?"
+        label="Dein Name"
+        placeholder="Namen eingeben"
+        help="Wie willst du genannt werden?"
+        validation="required|length:4"
+        :validation-messages="{ required: 'Ohne Name keine Party', length: 'Dein Name ist zu kurz' }"
+        validation-visibilty="live"
       />
+    
+      <FormKit
+        type="radio"
+        name="radios"
+        label="Anreise"
+        :options="[
+          'Bike',
+          'E-Scooter',
+          'U-/S-/Bahn',
+          'Zu Fuß',
+          'Luftkissenfahrzeug',
+        ]"
+      />
+
+      <FormKit type="group" name="dance" id="dance">
+
+      <FormKit
+        type="range"
+        name="level"
+        label="Tanz-Skills"
+        value="5"
+        min="1"
+        max="10"
+        step="1"
+        help="Wie schätzt du deine Tanz-Skills ein?"
+      />
+
+      
       <FormKit
         type="checkbox"
-        name="flavors"
-        label="Favorite ice cream flavors"
+        name="moves"
+        label="Tanz-Moves"
+        help="Was sind deine besten Tanz-Moves?"
         :options="{
-          'vanilla': 'Vanilla',
-          'chocolate': 'Chocolate',
-          'strawberry': 'Strawberry',
-          'mint-chocolate-chip': 'Mint Chocolate Chip',
-          'rocky-road': 'Rocky Road',
-          'cookie-dough': 'Cookie Dough',
-          'pistachio': 'Pistachio',
+          'boogie': 'Boogie',
+          'dab': 'Dab',
+          'headbanging': 'Headbanging',
+          'disco-fox': 'Disco Fox',
+          'macarena': 'Macarena',
+          'shuffle': 'Shuffle',
+          'rhabarberbarbara': 'Rhabarberbarbara',
         }"
         validation="required|min:2"
-      />
+      />  
+      </FormKit>
       
       <FormKit
         type="repeater"
-        name="invitees"
-        label="Invitees"
-        help="Who else should we invite to FormKit?"
+        name="songs"
+        label="Party-Songs"
+        help="Welche Songs sollen unbedingt gespiel werden?"
       >
         <FormKit
           type="text"
-          name="email"
-          label="Email"
-          validation="required|email"
+          name="song"
+          label="Dein Song"
+          validation="required"
         />
       </FormKit>
+
+      <FormKit
+        type="time"
+        label="Polnischen?"
+        min="1:00"
+        max="5:00"
+        help="Wann sollte man frühestens einen Polnischen machen?"
+      />
+
       <FormKit
         type="checkbox"
         name="agree"
-        label="I agree FormKit is the best form authoring framework."
+        label="Deine Party-Policy"
+        help="Ich verspreche meine besten Dance-Move zu zeigen"
+        validation="required"
       />
-      <pre class="font-mono text-sm p-4 bg-slate-100 mb-4">{{ value }}</pre>
+
+      <!-- <pre class="font-mono text-sm p-4 bg-slate-100 mb-4">{{ value }}</pre> -->
     </FormKit>
   </div>
 </template>
