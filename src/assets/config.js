ba89cb762b3fe46bdeda9ff5721d6b5650ac31e6
Julia Zuleger <julia.zuleger@init.de>
2024-06-18 17:24:09 +0200

    add customized theme

---
+++ src/assets/config.js
@@ -0,0 +1,35 @@
+export const travel = [
+  'Bike',
+  'E-Scooter',
+  'U-/S-/Bahn',
+  'Zu Fuß',
+  'Luftkissenfahrzeug',
+];
+
+export const locations = [
+  'Berlin',
+  'Hamburg',
+  'Köln',
+  'Leipzig',
+  'Mainz',
+  'München'
+];
+
+export const dance = {
+  'boogie': 'Boogie',
+  'dab': 'Dab',
+  'headbanging': 'Headbanging',
+  'disco-fox': 'Disco Fox',
+  'macarena': 'Macarena',
+  'shuffle': 'Shuffle',
+  'rhabarberbarbara': 'Rhabarberbarbara',
+};
+
+export const songs = [  
+  { label: 'Song 2', value: 'song-2' },
+  { label: 'Angels', value: 'angels' },
+  { label: 'Firestarter', value: 'firestarter' },
+  { label: 'Major Tom', value: 'major-tom' },
+  { label: 'Mr Brightside', value: 'mister-brightside' },
+  { label: 'Just Dance', value: 'just-dance' }
+];
